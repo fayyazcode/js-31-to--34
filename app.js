dc84a1909eb2d8a7acebff88edd02b4e851d830e
Fayyaz Alam <fayazalam.98@gmail.com>
2021-05-28 07:56:13 -0700

    Q 2 done

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // var math= new Date();
 
 // document.write(math)
+ //
+
+ //Q2
+//  var date= new Date();
+
+//  var month = date.getMonth()
+// console.log(month)
+//  var arr= ["January","February","March","April","May","June","July",
+//  "August","September","October","November","December"];
+// alert("Current Month : "+arr[month])
